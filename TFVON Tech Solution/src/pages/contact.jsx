f382cb11e3c535d4df3d38145af42b04dc33ca0c
Illyasviel-Von-Einzbern <fidelisalen@email.com>
2025-04-14 16:38:37 +0800

    contact-shits

--- TFVON Tech Solution/src/pages/contact.jsx
+++ TFVON Tech Solution/src/pages/contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       id="contact"
       className="flex items-center justify-center min-h-screen bg-[#00dbc5] text-white py-[100px] px-[20px]"
     >
-      <h1 className="text-[50px] font-bold">FOOTER</h1>
+      <h1 className="text-[50px] font-bold">CONTACT</h1>
     </section>
   );
 };
