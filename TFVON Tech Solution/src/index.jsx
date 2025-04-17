fa94c70442d88e623cdf04df83c92b97da68833b
Illyasviel-Von-Einzbern <fidelisalen@email.com>
2025-04-18 00:42:05 +0800

    RESPONSIVE

--- TFVON Tech Solution/src/index.jsx
+++ TFVON Tech Solution/src/index.jsx
@@ -5,8 +5,6 @@ import './assets/css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
